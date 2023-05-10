609066fb38067b03001e3b3f7836222c4b27ba92
Joelsoncrj <joelsonrocha.eng@gmail.com>
2023-05-10 15:24:50 -0300

    subscriber with led is working

--- main/uros_node.c
+++ main/uros_node.c
@@ -15,7 +15,10 @@
 #include <rclc/executor.h>
 #include <rmw_microros/rmw_microros.h>
 #include "uxr/client/config.h"
+#include "driver/gpio.h"
 
+#define LED_GPIO 2
+static uint8_t led_state = 0;
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 
@@ -38,10 +41,17 @@ void sub_led_callback(const void * msgin)
 {
 	const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
 	printf("Received: %d\n",  (int)  msg->data);
+	led_state = (int) msg->data;
+	gpio_set_level(LED_GPIO,led_state);
 }
 
 void uros_main_loop(void * arg)
 {
+	//led setup 
+	esp_rom_gpio_pad_select_gpio(LED_GPIO);
+	gpio_set_direction(LED_GPIO,GPIO_MODE_OUTPUT);
+	gpio_set_level(LED_GPIO,led_state);
+	//------
 	rcl_allocator_t allocator = rcl_get_default_allocator();
 	rclc_support_t support;
 
@@ -89,12 +99,10 @@ void uros_main_loop(void * arg)
 	RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
 	unsigned int rcl_wait_timeout = 1000;   // in ms
 	RCCHECK(rclc_executor_set_timeout(&executor, RCL_MS_TO_NS(rcl_wait_timeout)));
-	
+
 	// Add timer and subscriber to executor.
 	RCCHECK(rclc_executor_add_timer(&executor, &timer));
 	RCCHECK(rclc_executor_add_subscription(&executor, &led_subscriber, &recv_msg, &sub_led_callback, ON_NEW_DATA));
-
-
 	msg.data = 0;
 
 	while(1)
